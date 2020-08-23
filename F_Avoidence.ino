9682107c1351048c746141762b7b12cb9989f6bd
Jihwan Kim <raptorwoo@gmail.com>
2020-08-23 16:40:00 +0900

    modification to avoidence logic

--- F_Avoidence.ino
+++ F_Avoidence.ino
@@ -17,86 +17,78 @@
 void avoidence()
 {
   timedStop(15);
+  
+  //센싱
+  sense();
 
-  bool finishedMission = false;
-
-  //임무 수행시까지 센싱을 통해 장애물 회피
-  while(finishedMission != true)
+  compute_steering = 0;
+  compute_speed = 0.4;
+    
+  //장애물 15cm 전방에 있을 때
+  if(gfCenterDistance <= 150)
   {
-    //센싱
-    sense();
-
-    compute_steering = 0;
-    compute_speed = 0.4;
-    
-    //장애물 15cm 전방에 있을 때
-    if(gfCenterDistance <= 150)
-    {
-      HM10.write("---Object 15cm in front\n");
-      //후진
-      timedDrive(0, -1, 2, true, false);
+    HM10.write("---Object 15cm in front\n");
+    //후진
+    timedDrive(0, -1, 2, true, false);
       
-      //좌회전
-      while(gfCenterDistance <= 200)
-      {
-        sense();
-        timedDrive(-1, 0.6, 3, true, false);
-      }
-      timedDrive(-0.5, 0.6, 2, true, false);
+    //좌회전
+    while(gfCenterDistance <= 200)
+    {
+      sense();
+      timedDrive(-1, 0.6, 3, true, false);
+    }
+    timedDrive(-0.5, 0.6, 2, true, false);
 
-      //직진
-      timedDrive(0, 0.4, 3, true, 0);
+    //직진
+    timedDrive(0, 0.4, 3, true, 0);
       
-      //우회전
-      timedDrive(1, 0.6, 3, true, false);
-    }
-    //장애물 30cm 전방에 있을 때  
-    else if(gfCenterDistance <= 300)
-    {
-      HM10.write("---Object 30cm in front\n");
-
-      //좌회전
-      while(gfCenterDistance <= 300)
-      {
-        timedDrive(-1, 0.6, 1, true, false);
-      }
-      timedDrive(-1, 0.6, 2, true, false);   
-      //직진
-      timedDrive(0, 0.5, 3, true, 0);
-
-      //우회전 - 방향잡기
-      timedDrive(1, 0.5, 3, true, 0);
-    
-    }
-    //장애물 전방에 센싱되지 않음 || 멀리 있음  
-    else if(gfCenterDistance > 500)
-    {
-      HM10.write("---Object Not detected / far away\n");
-      timedDrive(-1, 0.6, 4, true, false);
-      timedDrive(0, 0.6, 2, true, false);
-      timedDrive(1, 0.6, 2, true, false);
-    }
-
-
-    //차선에 센서 닿았을 때 
-    if(gbLeftIR != detect_ir)
-    {
-      compute_steering = 1;
-      compute_speed = 0.1;
-      nextMove();
-      finishedMission = true;
-    } 
-    else if(gbRightIR != detect_ir)
-    {
-      nextMove();
-      compute_steering = -1;
-      compute_speed = 0.1;
-    }
-
-    //차선 감지 후 미션 종료로 간주
-    if(finishedMission == true){
-      break;
-    }
+    //우회전
+    timedDrive(1, 0.6, 3, true, false);
 
   }
+    
+  //장애물 30cm 전방에 있을 때  
+  else if(gfCenterDistance <= 300)
+  {
+    HM10.write("---Object 30cm in front\n");
+
+    //좌회전
+    while(gfCenterDistance <= 300)
+    {
+       timedDrive(-1, 0.6, 1, true, false);
+    }
+    timedDrive(-1, 0.6, 2, true, false);   
+    //직진
+    timedDrive(0, 0.5, 3, true, 0);
+
+    //우회전 - 방향잡기
+    timedDrive(1, 0.5, 3, true, 0);
+    
+  }
+  
+  //장애물 전방에 센싱되지 않음 || 멀리 있음  
+  else if(gfCenterDistance > 500)
+  {
+    HM10.write("---Object Not detected / far away\n");
+    timedDrive(-1, 0.6, 4, true, false);
+    timedDrive(0, 0.6, 2, true, false);
+    timedDrive(1, 0.6, 2, true, false);
+  }
+
+  
+  //차선에 센서 닿았을 때 
+  if(gbLeftIR != detect_ir)
+  {
+    compute_steering = 1;
+    compute_speed = 0.1;
+    nextMove();
+  } 
+  
+  else if(gbRightIR != detect_ir)
+  {
+    nextMove();
+    compute_steering = -1;
+    compute_speed = 0.1;
+  }
+  
 }
